bd911acb9df9b125e2dac8856c62d348aacd03df
mr-nsin <singhal.nitin94@gmail.com>
2025-02-11 17:37:03 +0530

    Add endpoint to get headers from request

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+from datetime import datetime
 import uvicorn
 
 app = FastAPI()
 
-
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+    
+    
 @app.get('/')
 async def read_root():
     return {"message": "Hello World"}
@@ -12,6 +18,27 @@ async def read_root():
 @app.get('/greet')
 async def greet_name(name: str, age: int) -> dict:
     return {"message": f"Hello {name}"}
+
+
+@app.post('/create_book')
+async def create_book(request: BookCreateModel):
+    return {
+        "title": request.title,
+        "author": request.author
+    }
+
+@app.get('/get_headers')
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None)
+):
+    request_headers = {}
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    request_headers["User-Agent"] = user_agent
+
+    return request_headers
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=9000, reload=True)
