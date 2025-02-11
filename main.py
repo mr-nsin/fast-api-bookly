1a3b8cfbaee6be58d2f551d1123377ea5ffcb225
= <=>
2025-02-11 23:17:44 +0530

    Create Books endpoint using fastapi framework with in memory database

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from src.books.routes import book_router
+
+
+app = FastAPI()
+
+app.include_router(book_router)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
---
+++ src/books/routes.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, status, HTTPException
+from typing import List
+from src.books.schemas import BookModel, BookUpdateModel
+from src.books.book_data import books
+
+
+book_router = APIRouter(prefix='/books', tags=['books'])
+
+
+@book_router.get("/", response_model=List[BookModel], status_code=status.HTTP_200_OK)
+async def get_all_books():
+    return books
+
+@book_router.post('/', response_model=BookModel, status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data: BookModel) -> dict:
+    new_book = book_data.model_dump()
+
+    return new_book
+
+@book_router.get('/{book_id}', response_model=BookModel, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            return book
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
+
+@book_router.put('/{book_id}', response_model=BookModel, status_code=status.HTTP_200_OK)
+async def update_book(book_id: int, book_update_data: BookUpdateModel) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            book.update(book_update_data.dict())
+            return book
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
+
+@book_router.delete('/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    for index, book in enumerate(books):
+        if book['id'] == book_id:
+            books.pop(index)
+            return
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
