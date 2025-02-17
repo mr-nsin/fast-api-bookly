2d925da409817e9cdb925d73323a9f5b19befcf0
mr-nsin <singhal.nitin94@gmail.com>
2025-02-17 15:30:18 +0530

    Fix issues in endpoint

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from typing import List
-from src.books.schemas import Book, BookUpdateModel
+from src.books.schemas import Book, BookUpdateModel, BookCreateModel
 from src.books.service import BookService
 from src.db.main import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,7 +14,7 @@ async def get_all_books(session: AsyncSession = Depends(get_session)):
     return books
 
 @book_router.post('/', response_model=Book, status_code=status.HTTP_201_CREATED)
-async def create_a_book(book_data: Book, session: AsyncSession = Depends(get_session)) -> dict:
+async def create_a_book(book_data: BookCreateModel, session: AsyncSession = Depends(get_session)) -> dict:
     new_book = await book_service.create_book(book_data, session)
     return new_book
 
@@ -26,7 +26,7 @@ async def get_book(book_uid: str, session: AsyncSession = Depends(get_session)) 
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
 
-@book_router.path('/{book_uid}')
+@book_router.patch('/{book_uid}')
 async def update_book(book_uid: str, book_update_data: BookUpdateModel, session: AsyncSession = Depends(get_session)) -> dict:
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
 
